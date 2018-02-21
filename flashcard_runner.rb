ed3c314c8ae38d752c9dbb23b39c90b749273da9
justinetroyke <jjtroyke@gmail.com>
2018-02-20 19:42:44 -0700

    iteration 5, built out runner but kept start method in round

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -6,26 +6,9 @@ require 'pry'
 
 @card_a = Card.new("How many teams are in MLB?", "30")
 @card_b = Card.new("Who is the 25th president of the United Sates?", "William McKinley")
-@card_c = Card.new("What is the most common household pet in the United States?", "Fish")
-@card_d = Card.new("In waht year was Colorado founded?", "1876")
-@deck_a = Deck.new(@cards)
-@round_1 = Round.new(@deck_a)
+@card_c = Card.new("What is the most popular household pet in the world?", "Fish")
+@card_d = Card.new("In what year was Colorado founded?", "1876")
+@deck = Deck.new([@card_a, @card_b])
+@round_1 = Round.new(@deck)
 
-def start
-  "Welcome! You're playing with 4 cards"
-  "-------------------------------------------------"
-  "This is card number #{} out of 4."
-
-end
-
-
-# cards = CardGenerator.new(filename).cards
-# @card_1 = Card.new("What is the capital of Alaska?", "Juneau")
-# @card_2 = Card.new("Approximately how many miles are in one
-#                    astronomical unit?", "93,000,000")
-# @guess_1 = Guess.new("Juneau", @card_1)
-# @guess_2 = Guess.new("93,000,000", @card_2)
-# @deck = Deck.new(@cards)
-# @round = Round.new(@deck)
-#
-# @round.start
+@round_1.start
--- lib/round.rb
+++ lib/round.rb
@@ -20,13 +20,38 @@ class Round
   end
 
   def number_correct
-    correct_guesses = @guesses.select do |guess|
-    guess.correct?
-    end
-    correct_guesses.length
+    @guesses.count(&:correct?)
   end
 
   def percent_correct
     ((number_correct.to_f / @guesses.count) * 100).to_i
+  end
+
+  def start
+    intro
+    card_loop
+    results
+  end
+
+  def intro
+    p "Welcome! You're playing with #{@deck.count} cards"
+    p "-------------------------------------------------"
+  end
+
+  def card_loop
+    @deck.cards.each.with_index(1) do |card, index|
+      p "This is card number #{index} out of #{@deck.count}."
+      p "Question: #{card.question}"
+      response = gets.strip
+      guess = record_guess(response)
+      sleep 2
+      p guess.feedback
+      sleep 2
+    end
+  end
+
+  def results
+    p "****** Game over! ******"
+    p "You had #{number_correct} correct guesses out of #{@deck.count} for a score of #{percent_correct}%."
   end
 end
